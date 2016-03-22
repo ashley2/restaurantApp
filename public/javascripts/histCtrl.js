09dc4d9cf34a38baed5bb288f49e69e3554a48da
Ashley Smith <ashley22@vt.edu>
2016-03-22 10:29:13 -0700

    updated scopes

--- public/javascripts/histCtrl.js
+++ public/javascripts/histCtrl.js
@@ -1,8 +1,69 @@
 angular.module('resApp')
 .controller('histCtrl', function($scope, ResService, $stateParams){
 
+$scope.reservations = [];
+
+  
+  ResService.getAll()
+  .then(function(res){
+
+    var reservations = res.data;
+    $scope.reservations = reservations;
+
+
+  }, function(err){
+    console.error(err);
+  });
+
+
   $scope.viewFull = null;
 
 
 
+
+$scope.viewRes = null;
+
+  $scope.editRes = function(reserv){
+    console.log(reserv);
+    $scope.viewRes = reserv;
+  }
+
+  $scope.saveEdit = function(){
+    $scope.viewRes = null;
+  }
+
+  $scope.update = function(viewRes){
+    ResService.update(viewRes)
+    .then(function(){
+      swal("Great!", "Your dest has been saved!", "success")
+    }, function(err){
+      console.log(err);
+    })
+  }
+
+  $scope.deleteRes = function(reserv){
+    console.log('delete')
+    swal({   title: "Are you sure?",   
+      text: "You will not be able to recover this reservations info!",
+      type: "warning",
+      showCancelButton: true, 
+      confirmButtonColor: "#DD6B55", 
+      confirmButtonText: "Yes, delete it!",
+      closeOnConfirm: false },
+      function(){   
+        ResService.delete(reserv)
+        .then(function(){
+          var index = $scope.reservations.indexOf(reserv);
+          $scope.reservations.splice(index, 1);
+          $scope.viewRes = null;
+          swal("Deleted!",
+           "Your reservations has been deleted.",
+           "success"); 
+        })
+      }, function(err){
+        console.log('err ' , err);
+      })
+  }
+
 })
+
--- public/javascripts/homeCtrl.js
+++ public/javascripts/homeCtrl.js
@@ -34,50 +34,6 @@ $scope.reservations = [];
   };
 
 
-
-  $scope.viewRes = null;
-
-  $scope.editRes = function(reserv){
-    console.log(reserv);
-    $scope.viewRes = reserv;
-  }
-
-  $scope.saveEdit = function(){
-    $scope.viewRes = null;
-  }
-
-  $scope.update = function(viewRes){
-    DestService.update(viewRes)
-    .then(function(){
-      swal("Great!", "Your dest has been saved!", "success")
-    }, function(err){
-      console.log(err);
-    })
-  }
-
-  $scope.deleteRes = function(reserv){
-    console.log('delete')
-    swal({   title: "Are you sure?",   
-      text: "You will not be able to recover this reservations info!",
-      type: "warning",
-      showCancelButton: true, 
-      confirmButtonColor: "#DD6B55", 
-      confirmButtonText: "Yes, delete it!",
-      closeOnConfirm: false },
-      function(){   
-        ResService.delete(reserv)
-        .then(function(){
-          var index = $scope.reservations.indexOf(reserv);
-          $scope.reservations.splice(index, 1);
-          $scope.viewRes = null;
-          swal("Deleted!",
-           "Your dest has been deleted.",
-           "success"); 
-        })
-      }, function(err){
-        console.log('err ' , err);
-      })
-  }
-
 })
+  
 
